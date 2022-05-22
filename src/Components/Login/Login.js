d6cfa2698c7ae73fdd86f3a547a5e81cb3e0dada
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-23 04:04:08 +0600

    login components css done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import GoogleButton from "../GoogleButton/GoogleButton";
 
 const Login = () => {
   return (
     <div>
-      <h1>this is login</h1>
-      <GoogleButton></GoogleButton>
+      <div className="input-container">
+        <div className="input-filed">
+          <form>
+            <h3 className="text-3xl font-medium m-10">
+              Please <span className="text-primary text-5xl">Login</span>
+            </h3>
+
+            <input
+              className="input-place"
+              type="email"
+              name=""
+              id=""
+              placeholder="Email"
+              required
+            />
+            <br />
+
+            <input
+              className="input-place"
+              type="password"
+              name=""
+              id=""
+              placeholder="Password"
+              required
+            />
+            <br />
+
+            <button className="signUp-button">Login</button>
+          </form>
+
+          <p className="login-link m-4">
+            New to this site?
+            <Link to="/signUp">Sign Up</Link>
+          </p>
+
+          <GoogleButton></GoogleButton>
+        </div>
+      </div>
     </div>
   );
 };
