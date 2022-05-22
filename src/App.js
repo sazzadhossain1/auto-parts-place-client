b2a114c5b373e1e6a9764d2b0a4525ee5e5e2ddf
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-22 18:21:56 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is App</h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Home/Banner.js
@@ -0,0 +1,123 @@
+import React from "react";
+import bannerPhoto from "../../image/banner/banner-img-1.png";
+import bannerPhoto1 from "../../image/banner/banner-img-2.png";
+import bannerPhoto2 from "../../image/banner/bestsell-thumb.png";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div class="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src="https://wp.brator.xyz/wp-content/uploads/banner-1.jpg"
+          className="w-full img"
+          alt=""
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" class="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" class="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+
+      <div id="slide2" className="carousel-item relative    w-full">
+        <div className="flex justify-around  items-center w-[100%]">
+          <div className="">
+            <p className="text-6xl text-sky-700">New Top Product</p>
+            <p className="text-6xl">
+              High <small className="text-primary">Quality</small>
+            </p>
+            <br />
+            <p className="text-3xl">
+              Excepteur sint occaecat cupidatat non proident
+            </p>
+
+            <p className="text-3xl">sunt in culpa qui officia.</p>
+          </div>
+
+          <img
+            src={bannerPhoto}
+            className="w-[35%] flex justify-between "
+            alt=""
+          />
+        </div>
+
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" class="btn btn-circle">
+            ❮
+          </a>
+
+          <a href="#slide3" class="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+
+      <div id="slide3" class="carousel-item relative w-full">
+        <div className="flex justify-around  items-center w-[100%]">
+          <div className="">
+            <p className="text-6xl text-sky-700">New Top Product</p>
+            <p className="text-6xl">
+              High <small className="text-primary">Quality</small>
+            </p>
+            <br />
+            <p className="text-3xl">
+              Excepteur sint occaecat cupidatat non proident
+            </p>
+
+            <p className="text-3xl">sunt in culpa qui officia.</p>
+          </div>
+
+          <img
+            src={bannerPhoto}
+            className="w-[35%] flex justify-between "
+            alt=""
+          />
+        </div>
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" class="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" class="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" class="carousel-item relative w-full">
+        <div className="flex justify-around  items-center w-[100%]">
+          <div className="">
+            <p className="text-6xl text-sky-700">New Top Product</p>
+            <p className="text-6xl">
+              High <small className="text-primary">Quality</small>
+            </p>
+            <br />
+            <p className="text-3xl">
+              Excepteur sint occaecat cupidatat non proident
+            </p>
+
+            <p className="text-3xl">sunt in culpa qui officia.</p>
+          </div>
+
+          <img
+            src={bannerPhoto}
+            className="w-[35%] flex justify-between "
+            alt=""
+          />
+        </div>
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" class="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" class="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
